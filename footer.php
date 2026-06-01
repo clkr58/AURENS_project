50b6a6d3b595bc5a24a7b5d9bc24567907a88604
yagmur <yagmurclkr58@hotmail.com>
2026-06-01 18:02:16 +0300

    İlk hafta şablon parçalama: header ve footer ayrıldı

---
+++ footer.php
@@ -0,0 +1,12 @@
+<div class="footer-bottom text-center py-3">
+  <p>© 2026 Aurens Perfume. Tüm Hakları Saklıdır.</p>
+</div>
+    </div>
+    <script src="js/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/waypoints/4.0.1/jquery.waypoints.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js"></script>
+    <script src="js/plugins.js"></script>
+    <script src="js/script.js"></script>
+  </body>
+</html>
---
+++ header.php
@@ -0,0 +1,47 @@
+<header id="header" class="site-header text-black bg-primary fixed-top">
+      <nav id="header-nav" class="navbar navbar-expand-lg">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="index.html">
+            <img src="images/main-logo.png" class="logo">
+          </a>
+          <button class="navbar-toggler d-flex d-lg-none order-3 p-2 border-0 shadow-none" type="button" data-bs-toggle="offcanvas" data-bs-target="#bdNavbar" aria-controls="bdNavbar" aria-expanded="false" aria-label="Toggle navigation">
+            <svg class="navbar-icon" width="50" height="50">
+              <use xlink:href="#navbar-icon"></use>
+            </svg>
+          </button>
+          <div class="offcanvas offcanvas-end" tabindex="-1" id="bdNavbar" aria-labelledby="bdNavbarOffcanvasLabel">
+            <div class="offcanvas-header px-4 pb-0">
+              <a class="navbar-brand" href="index.html">
+                <img src="images/main-logo.png" class="logo">
+              </a>
+              <button type="button" class="btn-close btn-close-black" data-bs-dismiss="offcanvas" aria-label="Close" data-bs-target="#bdNavbar"></button>
+            </div>
+            <div class="offcanvas-body">
+              <ul class="navbar-nav justify-content-end flex-grow-1 gap-5 pe-3">
+                <li class="nav-item">
+                  <a class="nav-link p-0" href="#intro">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link p-0" href="#overview">Overview</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link p-0" href="#video-player">Video</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link p-0" href="#testimonials">Reviews</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link p-0" href="#featured-products">Perfumes</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link p-0" href="#company-services">Services</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link p-0" href="#faqs">Faqs</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </header>
